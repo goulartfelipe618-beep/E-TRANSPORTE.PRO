8dc98debe2e36c9d0dc040f998a78cf7f74c3968
goulartfelipe618-beep <goulartfelipe618-beep@users.noreply.github.com>
2026-03-26 18:27:31 -0300

    Deploy GitHub Pages (gh-pages): 2026-03-26 18:27:31Z

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import AxusChat from "./components/AxusChat";
-import "./shell.css";
-
-const el = document.getElementById("axus-root");
-if (el) {
-  createRoot(el).render(<AxusChat />);
-}
